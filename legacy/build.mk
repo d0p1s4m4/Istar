3435c4e39c507bd126c31598cbb3d4d5282aa5f4
d0p1 <contact@d0p1.eu>
2022-09-28 11:09:23 +0200

    build: rebuild legacy when files are modified

--- legacy/build.mk
+++ legacy/build.mk
@@ -1,9 +1,13 @@
 LEGACY_AS	= nasm
 LEGACY_SRCS	= boot/boot.s stage2/stage2.s
+LEGACY_STAGE2_DEPS = $(addprefix legacy/stage2/, vga.s)
 LEGACY_OBJS	= $(addprefix legacy/, $(LEGACY_SRCS:.s=.bin))
 
 istar.bin: $(LEGACY_OBJS)
 	cat $^ /dev/zero | dd of=$@ bs=512 count=2880 iflag=fullblock
 
-legacy/%.bin: legacy/%.s
+legacy/boot/%.bin: legacy/boot/%.s
+	$(LEGACY_AS) -fbin -o $@ $<
+
+legacy/stage2/%.bin: legacy/stage2/%.s $(LEGACY_STAGE2_DEPS)
 	$(LEGACY_AS) -fbin -o $@ $<
